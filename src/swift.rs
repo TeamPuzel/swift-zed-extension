1960e0bf50dedd4fb4ca67bbfa392d75d0303238
TeamPuzel <puffed-01.blips@icloud.com>
2024-06-20 13:08:31 +0200

    Use path and the TOOLCHAINS variable

--- src/swift.rs
+++ src/swift.rs
@@ -1,21 +1,29 @@
 use zed_extension_api::{self as zed, Result};
 
-struct SwiftExtension {}
+struct SwiftExtension;
 
 impl zed::Extension for SwiftExtension {
     fn new() -> Self {
-        Self {}
+        Self
     }
 
     fn language_server_command(
         &mut self,
         _server_id: &zed::LanguageServerId,
-        _worktree: &zed::Worktree,
+        worktree: &zed::Worktree,
     ) -> Result<zed::Command> {
+        if let Some(path) = worktree.which("sourcekit-lsp") {
+            return Ok(zed::Command {
+                command: path,
+                args: Default::default(),
+                env: worktree.shell_env(),
+            });
+        }
+
         Ok(zed::Command {
             command: "/usr/bin/xcrun".into(),
             args: vec!["sourcekit-lsp".into()],
-            env: Default::default(),
+            env: worktree.shell_env(),
         })
     }
 }
